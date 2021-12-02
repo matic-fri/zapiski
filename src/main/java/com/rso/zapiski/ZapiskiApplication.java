57f5c566fd462d527c935a118795c93aa002e9c3
Matic Isovski <mi6568@student.uni-lj.si>
2021-12-02 12:43:42 +0100

    first commit

---
+++ src/main/java/com/rso/zapiski/ZapiskiApplication.java
@@ -0,0 +1,13 @@
+package com.rso.zapiski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapiskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapiskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rso/zapiski/ZapiskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rso.zapiski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapiskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
